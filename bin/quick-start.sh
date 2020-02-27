c08f55daa0d4c4f43855006275c439276d61c6e5
liuzhengyang <liuzhengyang75@gmail.com>
2020-02-28 00:15:35 +0800

    add quick-start.sh

---
+++ bin/quick-start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://maven.aliyun.com/repository/public/com/github/liuzhengyang/apm-boot/1.8/apm-boot-1.8-jar-with-dependencies.jar
+
+java -jar apm-boot-1.8-jar-with-dependencies.jar
