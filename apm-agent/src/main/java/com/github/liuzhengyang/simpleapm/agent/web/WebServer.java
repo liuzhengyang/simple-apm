b26813616ac9485e041f60ba7839d34a4dd7f038
liuzhengyang <liuzhengyang75@gmail.com>
2020-03-07 01:09:52 +0800

    add sockjs support and send debug message to sockjs client

--- apm-agent/src/main/java/com/github/liuzhengyang/simpleapm/agent/web/WebServer.java
+++ apm-agent/src/main/java/com/github/liuzhengyang/simpleapm/agent/web/WebServer.java
@@ -1,5 +1,9 @@
 package com.github.liuzhengyang.simpleapm.agent.web;
 
+import java.text.DateFormat;
+import java.time.Instant;
+import java.util.Date;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,9 +11,14 @@ import org.slf4j.LoggerFactory;
 import com.github.liuzhengyang.simpleapm.agent.command.decompiler.ProcyonCommand;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
 
 /**
@@ -20,9 +29,7 @@ import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
 
-    public static void main(String[] args) {
-        startWebServer(Vertx.vertx());
-    }
+    private static EventBus eventBus;
 
     public static void startWebServer(Vertx vertx) {
         logger.info("Start web server");
@@ -56,7 +63,42 @@ public class WebServer {
             });
         });
 
+        router.route("/static/*").handler(StaticHandler.create());
         // start a HTTP web server on port 8080
         vertx.createHttpServer().requestHandler(router).listen(8000);
+        startSockJS(vertx);
+    }
+
+    private static void startSockJS(Vertx vertx) {
+        Router router = Router.router(vertx);
+        // Allow events for the designated addresses in/out of the event bus bridge
+        BridgeOptions opts = new BridgeOptions()
+                .addInboundPermitted(new PermittedOptions().setAddress("chat.to.server"))
+                .addOutboundPermitted(new PermittedOptions().setAddress("chat.to.client"));
+
+        // Create the event bus bridge and add it to the router.
+        SockJSHandler ebHandler = SockJSHandler.create(vertx);
+        ebHandler.bridge(opts);
+        router.route("/eventbus/*").handler(ebHandler);
+
+        // Create a router endpoint for the static content.
+        router.route().handler(StaticHandler.create());
+
+        // Start the web server and tell it to use the router to handle requests.
+        vertx.createHttpServer().requestHandler(router).listen(8080);
+
+        eventBus = vertx.eventBus();
+
+        // Register to listen for messages coming IN to the server
+        eventBus.consumer("chat.to.server").handler(message -> {
+            // Create a timestamp string
+            String timestamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(Date.from(Instant.now()));
+            // Send the message back out to all clients with the timestamp prepended.
+            eventBus.publish("chat.to.client", timestamp + ": " + message.body());
+        });
+    }
+
+    public static void send(String msg) {
+        eventBus.publish("chat.to.client", msg);
     }
 }
--- apm-agent/src/main/resources/templates/source.ftl
+++ apm-agent/src/main/resources/templates/source.ftl
@@ -2,25 +2,68 @@
 <html xmlns:th="http://www.thymeleaf.org">
 <head>
     <title>ACE in Action</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/xterm@4.4.0/css/xterm.css" />
     <style type="text/css" media="screen">
         #editor {
+            float: left;
+            width: 1000px;
+            height: 2000px;
             position: absolute;
             top: 0;
             right: 0;
             bottom: 0;
             left: 0;
         }
+        #terminal {
+            margin-left: 1010px;
+            height: 2000px;
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            /*left: 0;*/
+        }
     </style>
 </head>
 <body>
 
 <div id="editor">${content}</div>
+<div id="terminal">
+
+</div>
 <!--<script src="/ace-builds/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.7/ace.js" integrity="sha256-C7DTYRJLG+B/VEzHGeoPMw699nsTQYPAXHKXZb+q04E=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="//cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/xterm@4.4.0/lib/xterm.min.js"></script>
+<script src="/static/js/vertx-eventbus.js"></script>
 <script>
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.session.setMode("ace/mode/java");
+</script>
+<script>
+    var term = new Terminal();
+    term.open(document.getElementById('terminal'));
+    term.writeln('\x1B[1;3;31msimple-apm\x1B[0m $ ')
+
+    var eb = new EventBus("http://localhost:8080/eventbus/");
+    eb.onopen = function () {
+        eb.registerHandler("chat.to.client", function (err, msg) {
+            $('#debugger').append(msg.body + "\n");
+            term.writeln(msg.body)
+        });
+    };
+
+    function send(event) {
+        if (event.keyCode == 13 || event.which == 13) {
+            var message = $('#input').val();
+            if (message.length > 0) {
+                eb.publish("chat.to.server", message);
+                $('#input').val("");
+            }
+        }
+    }
 </script>
 </body>
 </html>
